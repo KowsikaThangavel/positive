417b3d30941b5700725c6ef3844eb972917d0691
KowsikaThangavel <mail2kowsika95@gmail.com>
2016-07-20 10:09:57 +0530

    Add files via upload

---
+++ Positiveornegative.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package positiveornegative;
+
+import java.util.Scanner;
+public class Positiveornegative {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       {
+
+	            int n;
+
+	            Scanner s = new Scanner(System.in);
+
+	            System.out.print("Enter the number you want to check:");
+
+	            n = s.nextInt();
+
+	            if(n > 0)
+
+	            {
+
+	                System.out.println("The given number "+n+" is Positive");
+
+	            }
+
+	            else if(n < 0)
+
+	            {
+
+	                System.out.println("The given number "+n+" is Negative");
+
+	            }
+
+	            else
+
+	            {
+
+	                System.out.println("The given number "+n+" is neither Positive nor Negative ");
+
+	            }
+	            s.close();
+
+	        }
+
+	    }
+
+	}
+
+
+   
+    
+
